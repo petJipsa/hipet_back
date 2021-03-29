25fd32e9d172999be9c6badfbc0660bbcadc0b64
TitanV <a01098930999@gmail.com>
2021-03-29 22:26:02 +0900

    update - useless code

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,6 @@ const serviceAccount = require('../../service-account-file.json');
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
-
 export const verify = (async (token) => {
   let content;
 
@@ -15,21 +14,3 @@ export const verify = (async (token) => {
 
   return content;
 });
-
-
-/*
-admin
-  .auth()
-  .getUser('mJTCsG2whBXyiMKWqAGlG9anh5t2')
-  .then((userRecord) => {
-    // See the UserRecord reference doc for the contents of userRecord.
-    console.log(`Successfully fetched user data: ${JSON.stringify(userRecord)}`);
-    console.log(`Successfully fetched user data: ${userRecord.uid}`);
-    
-  })
-  .catch((error) => {
-    console.log('Error fetching user data:', error);
-  });
-
-  
-*/
