2dd6446b5f66f565de6fbff4aeaf44f7a86df8ce
TitanV <a01098930999@gmail.com>
2021-06-07 17:17:12 +0900

    update  - filter

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,11 +10,7 @@ const storage = multer.diskStorage({
 const fileFilter = async (req, file, cb) => {
   let typeArray = file.mimetype.split('/');
   let fileType = typeArray[1];
-  if (fileType == 'mkv' || fileType == 'avi' || fileType == 'mp4' || fileType == 'mpg' || fileType == 'flv' || fileType == 'wmv' || fileType == 'asf' || fileType == 'asx' || fileType == 'ogm' || fileType == 'ogv' || fileType == 'mov') {
-    cb(null, true);
-  }else{
-    cb(null, false)
-  }
+  cb(null, true);
 }
 
 const upload = multer({ storage: storage, fileFilter: fileFilter });
